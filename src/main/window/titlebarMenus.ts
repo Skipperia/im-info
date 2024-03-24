838f3d03dc6dc4dcd55f5675c3e59f221e4a5403
Alex Kipper <kippera27@gmail.com>
2024-03-24 14:23:03 +0200

    added basic todo

--- src/main/window/titlebarMenus.ts
+++ src/main/window/titlebarMenus.ts
@@ -45,41 +45,6 @@ const titlebarMenus: TitlebarMenu[] = [
         action: 'undo',
         shortcut: 'Ctrl+Z',
       },
-      {
-        name: 'Redo',
-        action: 'redo',
-        shortcut: 'Ctrl+Y',
-      },
-      {
-        name: '__',
-      },
-      {
-        name: 'Cut',
-        action: 'cut',
-        shortcut: 'Ctrl+X',
-      },
-      {
-        name: 'Copy',
-        action: 'copy',
-        shortcut: 'Ctrl+C',
-      },
-      {
-        name: 'Paste',
-        action: 'paste',
-        shortcut: 'Ctrl+V',
-      },
-      {
-        name: 'Delete',
-        action: 'delete',
-      },
-      {
-        name: '__',
-      },
-      {
-        name: 'Select All',
-        action: 'select_all',
-        shortcut: 'Ctrl+A',
-      },
     ],
   },
   {
--- src/renderer/appPreload.tsx
+++ src/renderer/appPreload.tsx
@@ -3,5 +3,8 @@ import '@main/window/windowPreload';
 // Say something
 console.log('[IM-Info] : Preload execution started');
 
+
 window.addEventListener('DOMContentLoaded', () => {
+  const app = document.getElementById('app');
+  //todo: stuff to do when page loaded
 });
